33882a447817a19c671d0a5c1d504166ee2aab09
chunyu <gechunyu92@hotmail.com>
2020-12-29 14:13:29 +0800

    change function name

---
+++ markEGG.m
@@ -0,0 +1,12 @@
+function [peakidx, dpeakidx, maxContact, noContact, opening] = ...
+    markEGG(file, interval, ampfloor, freqceiling, maxThresh, noThresh, openThresh)
+[sig, Fs] = audioread(file);
+startTime = interval(1);
+endTime = interval(2);
+sig = sig(round(startTime*Fs):round(endTime*Fs));
+peakidx = findEGGpeak(sig, Fs, ampfloor, freqceiling);
+dpeakidx = detectdEGGpeak(sig, peakidx);
+maxContact = getThresh(sig, peakidx, maxThresh);
+noContact = getThresh(sig, peakidx, noThresh);
+opening = getThresh(sig, peakidx, openThresh);
+end
