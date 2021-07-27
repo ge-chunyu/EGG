deb8d4fae78cc891f7736d61ab00c73f63d5dfc4
chunyu <gechunyu92@hotmail.com>
2021-07-27 15:51:47 +0800

    change path

--- analyzeEGG.m
+++ analyzeEGG.m
@@ -1,16 +1,16 @@
 function analyzeEGG()
-    %path = uigetdir(cd, "Choose folder");
-    speaker = "M14";
-    path = "/Users/Chunyu/Desktop/suzhou/egg/";
-    path = strcat(path, speaker);
-    path = strcat(path, filesep);
+    path = uigetdir(cd, "Choose EGG folder");
+    %speaker = "M14";
+    %path = "/Users/Chunyu/Desktop/suzhou/egg/";
+    %path = strcat(path, speaker);
+    %path = strcat(path, filesep);
     files = dir(strcat(path, "*.wav"));
     fileNum = length(files);
 
-    %labelPath = uigetdir(cd, "Choose TextGrid folder");
-    labelPath = "/Users/chunyu/Desktop/suzhou/";
-    labelPath = strcat(labelPath, speaker);
-    labelPath = strcat(labelPath, filesep);
+    labelPath = uigetdir(cd, "Choose TextGrid folder");
+    %labelPath = "/Users/chunyu/Desktop/suzhou/";
+    %labelPath = strcat(labelPath, speaker);
+    %labelPath = strcat(labelPath, filesep);
     labelFiles = dir(strcat(labelPath, "*.TextGrid"));
 
     % specify coefficients
