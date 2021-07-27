41ca572d92c4e4df639e78f617dd03c3af13ba22
chunyu <gechunyu92@hotmail.com>
2021-07-27 15:49:34 +0800

    calculate sqh

--- analyzeEGG.m
+++ analyzeEGG.m
@@ -1,29 +1,29 @@
 function analyzeEGG()
-    path = uigetdir(cd, "Choose EGG folder");
-    %speaker = "F20";
-    %path = "/Users/Chunyu/Desktop/suzhou/egg/";
-    %path = strcat(path, speaker);
-    %path = strcat(path, filesep);
+    %path = uigetdir(cd, "Choose folder");
+    speaker = "M14";
+    path = "/Users/Chunyu/Desktop/suzhou/egg/";
+    path = strcat(path, speaker);
+    path = strcat(path, filesep);
     files = dir(strcat(path, "*.wav"));
     fileNum = length(files);
 
-    labelPath = uigetdir(cd, "Choose TextGrid folder");
-    %labelPath = "/Users/chunyu/Desktop/suzhou/";
-    %labelPath = strcat(labelPath, speaker);
-    %labelPath = strcat(labelPath, filesep);
-    %labelFiles = dir(strcat(labelPath, "*.TextGrid"));
+    %labelPath = uigetdir(cd, "Choose TextGrid folder");
+    labelPath = "/Users/chunyu/Desktop/suzhou/";
+    labelPath = strcat(labelPath, speaker);
+    labelPath = strcat(labelPath, filesep);
+    labelFiles = dir(strcat(labelPath, "*.TextGrid"));
 
     % specify coefficients
     ampfloor = 0.1;
-    freqceiling = 450;
+    freqceiling = 300;
     maxThresh = 0.9;
     noThresh = 0.1;
     openThresh = 0.25;
     nPoint = 12;
     
-    data = table([speaker], ["a"],[0],[0],[0],[0],[0],[0],[0],[0],...
+    data = table([speaker], ["a"],[0],[0],[0],[0],[0],[0],[0],[0],[0],...
                  'VariableNames',{'filename', 'label', 'normTime',...
-                 'point', 'nFoc', 'nFov', 'nCqc', 'nSqc', 'nCqh', 'nPic'});
+                 'point', 'nFoc', 'nFov', 'nCqc', 'nSqc', 'nCqh', 'nPic', 'nSqh'});
     
     for i = 1:fileNum
         tokenPath = strcat(path, files(i).name);
@@ -46,13 +46,13 @@ function analyzeEGG()
             [peakidx, dpeakidx, maxContact, noContact, opening, pic] = ...
             markEGG(sig, Fs, startTime, endTime, ampfloor, freqceiling, maxThresh, noThresh, openThresh);
             if length(peakidx) >= 13
-                [cycle, dEGGpks, fo_c, fo_v, cq_c, sq_c, cq_h] = calcPara(Fs, dpeakidx, maxContact, opening, 4);
-                [normTime, nFoc, nFov, nCqc, nSqc, nCqh, nPic] = normPara(startTime, endTime, nPoint, cycle, fo_c, fo_v, cq_c, sq_c, cq_h, pic);
+                [cycle, dEGGpks, fo_c, fo_v, cq_c, sq_c, cq_h, sq_h] = calcPara(Fs, dpeakidx, maxContact, opening, noContact, 4);
+                [normTime, nFoc, nFov, nCqc, nSqc, nCqh, nPic, nSqh] = normPara(startTime, endTime, nPoint, cycle, fo_c, fo_v, cq_c, sq_c, cq_h, pic, sq_h);
                 filename = repmat(files(i).name, 12, 1);
                 label = repmat(rhyme, 12, 1);
                 point = [1:12]';
                 normTime = round(normTime', 2);
-                temp = table(filename, label, normTime, point, nFoc, nFov, nCqc, nSqc, nCqh, nPic);
+                temp = table(filename, label, normTime, point, nFoc, nFov, nCqc, nSqc, nCqh, nPic, nSqh);
                 data = vertcat(data, temp);  
                 fprintf("%s done!\n", files(i).name)
             end
